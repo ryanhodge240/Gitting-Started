9bd21b04df8ad5856965605ef4abce9fd487abb4
Ryan Hodge <ryanhodge240@gmail.com>
2021-09-16 09:59:22 -0400

    Created the main python file for this repo

---
+++ src/main.py
@@ -0,0 +1,15 @@
+################################################################
+# This is an example of some code within a Github repository.
+# Please take a look through this code and modify it to practice
+# your new knowledge of git!
+################################################################
+
+"""The main python file for this repo"""
+
+def main():
+    """The main function for the program"""
+
+    print('Hello, world!')
+
+if __name__ == '__main__':
+    main()
